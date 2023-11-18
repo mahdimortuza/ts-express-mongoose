fcad53bdf5b68a8041a19ae463e4ea4d2fccc71c
Mahdi Mortuza <mahdi.webx@gmail.com>
2023-11-18 23:59:26 +0600

    successfully creating first api

---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { StudentServices } from './student.service';
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const { student: studentData } = req.body;
+
+    const result = await StudentServices.createStudentIntoDB(studentData);
+
+    res.status(200).json({
+      status: true,
+      message: 'student has created successfully.',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const StudentControllers = {
+  createStudent,
+};
